83d40e74c4b4c5d567d8b90470e49b825929bf86
Antonino Alampi <Alampiantonino99@gmail.com>
2023-07-19 23:09:46 +0200

    feat: added Navbar component and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
