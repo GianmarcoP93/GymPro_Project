4b68ae4d3a7fbd73beb24a5ba2a9912bf1d01062
Gianmarco <gianmarco.pesola@live.it>
2023-07-26 14:35:18 +0200

    component/navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,12 +6,12 @@ export const Navbar = () => {
     <nav className="flex bg-[#393b44] w-[100%] justify-between ">
       <img src={logo} alt="" className=" max-h-[65px] ml-8 my-1" />
       <div className=" text-white-100 gap-5 flex items-center hover:decoration-solid ">
-        <Link className=" hover:border-b hover:text-yellow-100 ">Home</Link>
-        <Link className="hover:border-b hover:text-yellow-100">
+        <Link className=" hover:border-b hover:text-yellow-200 ">Home</Link>
+        <Link className="hover:border-b hover:text-yellow-200">
           Aziende Partner
         </Link>
-        <Link className="hover:border-b hover:text-yellow-100">Chi siamo</Link>
-        <Link className="hover:border-b hover:text-yellow-100">FAQ</Link>
+        <Link className="hover:border-b hover:text-yellow-200">Chi siamo</Link>
+        <Link className="hover:border-b hover:text-yellow-200">FAQ</Link>
       </div>
       <div className="flex items-center ">
         <button className=" mr-8 text-white bg-secondary-100 px-10 py-2 border-black border-[1px] rounded-[50px] hover:bg-secondary-200  hover:text-white-100">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#14161F",
-        },
+        //sfondo scuro
+        primary: "#14161F",
+        //arancione-100 normale, 200:hover
         secondary: {
           100: "#FF721B",
           200: "#F87A2C",
         },
+        //yellow-100 normale, 200:hover
         yellow: {
           100: "#E5BB39",
-          200: "#DEAB0B",
+          200: "#EDCD6B",
         },
+        //verde
         green: "#167b20",
+        //grigio
         gray: "#393b44",
+        //bianco-100 normale, 200:hover
         white: {
           100: "#dfdfdf",
           200: "#afafaf",
         },
+        //sfondo 2 sezione chiara
         bgwhite: "#F1F2F8",
+        //colore rosso
+        red: "#D60000",
       },
-      red: "#D60000",
     },
   },
   plugins: [],
