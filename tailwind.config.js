5aec4d6f9beca1e5e9f5dafd8755422384f0e63c
Gianmarco <gianmarco.pesola@live.it>
2023-07-26 14:05:33 +0200

    bugfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "primary-color": "#3903DE",
-    },
+    colors: {},
   },
   plugins: [],
 };
