eceb5889550a8c5f5811f7d2f9c21846302e0e3e
Andrea Izzo <aizzo450@gmail.com>
2023-07-26 19:20:15 +0200

    feat:added component footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LoginPage from "./Pages/LoginPage";
 import Button from "./Component/Button";
+import Footer from "./Component/Footer"
+
 
 const App = () => {
   return (
     <>
-     <LoginPage />
+     <Footer />
     </>
   );
 };
