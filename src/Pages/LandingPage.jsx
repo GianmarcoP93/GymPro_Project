4a48f5430d82aceb619659af80f2199ac53a4652
Antonino Alampi <Alampiantonino99@gmail.com>
2023-07-26 21:57:50 +0200

    Feat: created third section and Landing Page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,11 +2,18 @@ import { HeroSection } from "../Component/HeroSection";
 import { Navbar } from "../Component/Navbar";
 import { SecondSection } from "../Component/SecondSection";
 import { SectionDivider } from "../Component/SectionDivider";
+import { ThirdSection } from "../Component/ThirdSection";
 
 export const LandingPage = () => {
   return (
     <>
       <Navbar />
+      <HeroSection />
+      <SectionDivider />
+      <SecondSection />
+      <SectionDivider />
+      <ThirdSection />
+      <SectionDivider />
     </>
   );
 };
