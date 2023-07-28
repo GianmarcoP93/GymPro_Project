0afd5d86bd18e303f91d5a8d0fe99b8b217eb707
Simone Sbrilly <simonesbrilli@gmail.com>
2023-07-28 22:52:29 +0200

    mbi trend is working

--- src/Pages/UserDashboard.jsx
+++ src/Pages/UserDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Bar,
   BarChart,
@@ -13,84 +13,104 @@ import clsx from "clsx";
 
 const data = [
   {
-    month: "Genn",
-    savings: 4000,
-    checkings: 500,
+    month: "Gen",
+    bmi: 0,
   },
   {
-    month: "Febb",
-    savings: 6000,
-    checkings: 8000,
+    month: "Feb",
+    bmi: 0,
   },
   {
     month: "Mar",
-    savings: 2000,
-    checkings: 200,
+    bmi: 0,
   },
   {
     month: "Apr",
-    savings: 3000,
-    checkings: 1000,
+    bmi: 0,
   },
   {
     month: "Mag",
-    savings: 6000,
-    checkings: 1500,
+    bmi: 0,
   },
   {
     month: "Giu",
-    savings: 8000,
-    checkings: 1200,
+    bmi: 0,
   },
   {
     month: "Lug",
-    savings: 4000,
-    checkings: 500,
+    bmi: 0,
   },
   {
     month: "Ago",
-    savings: 4000,
-    checkings: 500,
+    bmi: 0,
   },
   {
-    month: "Sett",
-    savings: 4000,
-    checkings: 500,
+    month: "Set",
+    bmi: 0,
   },
   {
     month: "Ott",
-    savings: 4000,
-    checkings: 500,
+    bmi: 0,
   },
   {
     month: "Nov",
-    savings: 4000,
-    checkings: 500,
+    bmi: 0,
   },
   {
     month: "Dic",
-    savings: 4000,
-    checkings: 500,
+    bmi: 0,
   },
 ];
 
 export const UserDashboard = () => {
-  const [state, setState] = useState("");
+  const [formData, setFormData] = useState({
+    month: "",
+    weight: "",
+    height: "",
+    bmi: "",
+  });
 
-  const handleInputChange = (e) => {
-    const input = e.target.value;
+  const [chartData, setChartData] = useState(data);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleSelectChange = (e) => {
-    const value = e.target.value;
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    if (value === "default") {
-      const defaultValue = "savings";
-      setState(defaultValue);
-    } else if (value === "weight") {
-      const weightValue = "checkings";
-      setState(weightValue);
+    const month = formData.month;
+    const weight = parseFloat(formData.weight);
+    const height = parseFloat(formData.height);
+
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+      console.log("Inserisci i valori validi per il peso e l'altezza");
+      return;
     }
+
+    const bmi = weight / ((height / 100) * (height / 100));
+
+    myFunction(month, weight, height, bmi);
+
+    const newData = {
+      month: month,
+      bmi: bmi.toFixed(2),
+    };
+
+    setChartData((prevData) =>
+      prevData.map((item) => (item.month === month ? newData : item))
+    );
+  };
+
+  const myFunction = (month, height, weight, bmi) => {
+    console.log("mese: ", month);
+    console.log("peso: ", height);
+    console.log("altezza: ", weight);
+    console.log("BMI: ", bmi);
   };
 
   return (
@@ -98,41 +118,78 @@ export const UserDashboard = () => {
       <div className="p-10 shadow-lg">
         <h2 className="text-2xl text-yellow-500">I tuoi obbiettivi</h2>
         <div className="shadow-lg mt-4 rounded-lg p-3">
-          <h3>Quanto pesi oggi?</h3>
-          <div className="flex flex-row gap-20 mt-10">
+          <h3>Inserisci i tuoi dati</h3>
+          <form
+            onSubmit={handleSubmit}
+            name="userForm"
+            className="flex flex-row gap-20 mt-10"
+          >
             <div>
-              <label htmlFor="peso">Inserisci il tuo peso</label>
-              <input name="peso" type="text" className="ml-5" />
+              <label htmlFor="peso" name="weight">
+                Inserisci il tuo peso
+              </label>
+              <input
+                value={formData.weight}
+                onChange={handleChange}
+                name="weight"
+                type="text"
+                className="ml-5"
+              />
             </div>
             <div>
-              <label htmlFor="data">Inserisci la data</label>
-              <input type="date" className="ml-5" />
+              <label htmlFor="height" name="height">
+                Inserisci la tua statura
+              </label>
+              <input
+                value={formData.height}
+                onChange={handleChange}
+                name="height"
+                type="text"
+                className="ml-5"
+              />
             </div>
-          <div>
-            <button>Invia</button>
-          </div>
-          </div>
+            <div>
+              <label htmlFor="month" name="month">
+                Inserisci il mese
+              </label>
+              <select
+                value={formData.month}
+                onChange={handleChange}
+                name="month"
+              >
+                <option value="null">---</option>
+                <option value="Gen">Gennaio</option>
+                <option value="Feb">Febbraio</option>
+                <option value="Mar">Marzo</option>
+                <option value="Apr">Aprile</option>
+                <option value="Mag">Maggio</option>
+                <option value="Giu">Giugno</option>
+                <option value="Lug">Luglio</option>
+                <option value="Ago">Agosto</option>
+                <option value="Set">Settembre</option>
+                <option value="Ott">Ottobre</option>
+                <option value="Nov">Novembre</option>
+                <option value="Dic">Dicembre</option>
+              </select>
+            </div>
+            <div>
+              <button type="submit">Invia</button>
+            </div>
+          </form>
         </div>
         <div className="shadow-lg mt-4 rounded-lg">
           <div className="p-3">
             <h3>Il tuo andamento</h3>
-            <label htmlFor="select">Seleziona</label>
-            <select name="select" onChange={handleSelectChange}>
-              <option value="default">---</option>
-              <option value="weight">Peso</option>
-              <option value="training">Allenamento</option>
-            </select>
             <div className="flex flex-col items-center">
               <p>Stacked Bar Charts</p>
               <div>
-                <BarChart data={data} width={1500} height={300}>
+                <BarChart data={chartData} width={1500} height={300}>
                   <CartesianGrid></CartesianGrid>
                   <XAxis dataKey="month"></XAxis>
-                  <YAxis dataKey="savings"></YAxis>
+                  <YAxis dataKey="bmi"></YAxis>
                   <Tooltip></Tooltip>
                   <Legend></Legend>
-                  <Bar dataKey={state} fill="violet"></Bar>
-                  <Bar dataKey="checkings" fill="green"></Bar>
+                  <Bar dataKey="bmi" fill="violet"></Bar>
                 </BarChart>
               </div>
             </div>
