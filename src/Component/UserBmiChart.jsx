6f543cf8949b90cf0942ee3d8143a7e9006555be
Simone Sbrilly <simonesbrilli@gmail.com>
2023-07-31 15:47:42 +0200

    file UserBmiChart.jsx created and and the code has been moved out from UserDashboard.jsx file

---
+++ src/Component/UserBmiChart.jsx
@@ -0,0 +1,220 @@
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+import clsx from "clsx";
+
+const data = [
+  {
+    month: "Gen",
+    bmi: 0,
+  },
+  {
+    month: "Feb",
+    bmi: 0,
+  },
+  {
+    month: "Mar",
+    bmi: 0,
+  },
+  {
+    month: "Apr",
+    bmi: 0,
+  },
+  {
+    month: "Mag",
+    bmi: 0,
+  },
+  {
+    month: "Giu",
+    bmi: 0,
+  },
+  {
+    month: "Lug",
+    bmi: 0,
+  },
+  {
+    month: "Ago",
+    bmi: 0,
+  },
+  {
+    month: "Set",
+    bmi: 0,
+  },
+  {
+    month: "Ott",
+    bmi: 0,
+  },
+  {
+    month: "Nov",
+    bmi: 0,
+  },
+  {
+    month: "Dic",
+    bmi: 0,
+  },
+];
+
+export const UserBmiChart = () => {
+  const [formData, setFormData] = useState({
+    month: "",
+    weight: "",
+    height: "",
+    bmi: "",
+  });
+
+  const [chartData, setChartData] = useState(() => {
+    const storedChartData = localStorage.getItem("chartData");
+    if (storedChartData) {
+      try {
+        return JSON.parse(storedChartData);
+      } catch (error) {
+        console.error(
+          "Errore durante il parsing dei dati dal localStorage:",
+          error
+        );
+      }
+    }
+    return data;
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const month = formData.month;
+    const weight = parseFloat(formData.weight);
+    const height = parseFloat(formData.height);
+
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+      console.log("Inserisci i valori validi per il peso e l'altezza");
+      return;
+    }
+
+    const bmi = weight / ((height / 100) * (height / 100));
+
+    myFunction(month, weight, height, bmi);
+
+    const newData = {
+      month: month,
+      bmi: bmi.toFixed(2),
+    };
+
+    const updateChartData = chartData.map((item) =>
+      item.month === month ? newData : item
+    );
+
+    setChartData(updateChartData);
+
+    localStorage.setItem("chartData", JSON.stringify(updateChartData));
+  };
+
+  const myFunction = (month, height, weight, bmi) => {
+    console.log("mese: ", month);
+    console.log("peso: ", height);
+    console.log("altezza: ", weight);
+    console.log("BMI: ", bmi);
+  };
+
+  return (
+    <div className=" bg-slate-100">
+      <div className="p-10 shadow-lg">
+        <h2 className="text-2xl text-yellow-500">I tuoi obbiettivi</h2>
+        <div className="shadow-lg mt-4 rounded-lg p-3">
+          <h3>Inserisci i tuoi dati</h3>
+          <form
+            onSubmit={handleSubmit}
+            name="userForm"
+            className="flex flex-row gap-20 mt-10"
+          >
+            <div>
+              <label htmlFor="peso" name="weight">
+                Inserisci il tuo peso
+              </label>
+              <input
+                value={formData.weight}
+                onChange={handleChange}
+                name="weight"
+                type="text"
+                className="ml-5"
+              />
+            </div>
+            <div>
+              <label htmlFor="height" name="height">
+                Inserisci la tua statura
+              </label>
+              <input
+                value={formData.height}
+                onChange={handleChange}
+                name="height"
+                type="text"
+                className="ml-5"
+              />
+            </div>
+            <div>
+              <label htmlFor="month" name="month">
+                Inserisci il mese
+              </label>
+              <select
+                value={formData.month}
+                onChange={handleChange}
+                name="month"
+              >
+                <option value="null">---</option>
+                <option value="Gen">Gennaio</option>
+                <option value="Feb">Febbraio</option>
+                <option value="Mar">Marzo</option>
+                <option value="Apr">Aprile</option>
+                <option value="Mag">Maggio</option>
+                <option value="Giu">Giugno</option>
+                <option value="Lug">Luglio</option>
+                <option value="Ago">Agosto</option>
+                <option value="Set">Settembre</option>
+                <option value="Ott">Ottobre</option>
+                <option value="Nov">Novembre</option>
+                <option value="Dic">Dicembre</option>
+              </select>
+            </div>
+            <div>
+              <button type="submit">Invia</button>
+            </div>
+          </form>
+        </div>
+        <div className="shadow-lg mt-4 rounded-lg">
+          <div className="p-3">
+            <h3>Il tuo andamento</h3>
+            <div className="flex flex-col items-center">
+              <p>Stacked Bar Charts</p>
+              <div>
+                <BarChart data={chartData} width={1500} height={300}>
+                  <CartesianGrid></CartesianGrid>
+                  <XAxis dataKey="month"></XAxis>
+                  <YAxis dataKey="bmi"></YAxis>
+                  <Tooltip></Tooltip>
+                  <Legend></Legend>
+                  <Bar dataKey="bmi" fill="#F87A2C"></Bar>
+                </BarChart>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserBmiChart;
