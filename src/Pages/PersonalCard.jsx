33a9142ab5bdaa1ce04249207f2062c5f0e7d450
Gianmarco <gianmarco.pesola@live.it>
2023-08-03 14:29:34 +0200

    feat: added map and modal img

--- src/Pages/PersonalCard.jsx
+++ src/Pages/PersonalCard.jsx
@@ -1,45 +1,154 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+
 export const PersonalCard = () => {
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [showBody, setShowBody] = useState(true);
+
+  Modal.setAppElement("#root");
+
+  const scheda = [
+    {
+      giorno: "Giorno 1",
+      exercises: [
+        {
+          id: 0,
+          name: "Panca piana bil",
+          img: "https://media.tenor.com/0hoNLcggDG0AAAAC/bench-press.gif",
+        },
+        {
+          id: 1,
+          name: "Panca inclinata bil",
+          img: "https://hips.hearstapps.com/menshealth-uk/main/assets/incline-bench-press.gif?crop=1.00xw:0.845xh;0,0.137xh&resize=980:*",
+        },
+      ],
+    },
+    {
+      giorno: "Giorno 2",
+      exercises: [
+        {
+          id: 0,
+          name: "Panca piana bil",
+          img: "https://media.tenor.com/0hoNLcggDG0AAAAC/bench-press.gif",
+        },
+        {
+          id: 1,
+          name: "Panca inclinata bil",
+          img: "https://hips.hearstapps.com/menshealth-uk/main/assets/incline-bench-press.gif?crop=1.00xw:0.845xh;0,0.137xh&resize=980:*",
+        },
+      ],
+    },
+    {
+      giorno: "Giorno 3",
+      exercises: [
+        {
+          id: 0,
+          name: "Panca piana bil",
+          img: "https://media.tenor.com/0hoNLcggDG0AAAAC/bench-press.gif",
+        },
+        {
+          id: 1,
+          name: "Panca inclinata bil",
+          img: "https://hips.hearstapps.com/menshealth-uk/main/assets/incline-bench-press.gif?crop=1.00xw:0.845xh;0,0.137xh&resize=980:*",
+        },
+      ],
+    },
+  ];
+
+  const closeModal = () => {
+    setSelectedExercise(null);
+  };
+
+  const closeBody = () => {
+    setShowBody(!showBody);
+  };
+
+  const openExerciseModal = (exercise) => {
+    setSelectedExercise(exercise);
+  };
+
   return (
     <>
-      <div className="bg-white-00 border border-slate-300 rounded-xl w-full mx-auto bg-gray ">
-        <table className="w-full ">
-          <thead>
-            <tr className="border-bot border-slate-300 text-secondary-100 font-roboto font-bold text-xl">
-              <th className="py-5">Lista utenti</th>
-              <th>Rinnovato il</th>
-              <th>Scadenza scheda</th>
-              <th>N. Tessera</th>
-              <th>Scadenza abbonamento</th>
-            </tr>
-          </thead>
-          <tbody className="w-full font-montserrat  ">
-            <tr className="border-t border-slate-300 text-white-100 text-center">
-              <td className="py-5">
-                <button className="underline decoration-1 hover:text-secondary-300">
-                  aaaa
-                </button>
-              </td>
+      <div>
+        <div className="flex justify-center mt-10">
+          <h1 className="text-secondary-100 text-3xl">Scheda personale</h1>
+        </div>
+        <div className="bg-white-00  w-full mx-auto  flex flex-col gap-20">
+          {scheda &&
+            scheda.map((item, index) => {
+              return (
+                <table
+                  className={`w-full bg-gray border border-slate-300 ${
+                    showBody ? "transparent" : ""
+                  }`}
+                  key={index}
+                >
+                  <caption
+                    className="border-slate-300 text-secondary-100 font-roboto font-bold text-xl py-5 bg-gray border rounded-t-xl"
+                    onClick={closeBody}
+                  >
+                    <button
+                      className="w-full text-3xl font-roboto font-bold"
+                      onClick={closeBody}
+                    >
+                      {item.giorno}
+                    </button>
+                  </caption>
+                  <thead>
+                    <tr className="border-slate-300 text-secondary-100 font-roboto font-bold text-xl">
+                      <th className="py-5 w-1/5">Esercizi</th>
+                      <th className="w-1/5">Set</th>
+                      <th className="w-1/5">Ripetizioni</th>
+                      <th className="w-1/5">Rec</th>
+                      <th className="w-1/5">Kg</th>
+                    </tr>
+                  </thead>
+                  <tbody className="w-full font-montserrat border-t border-slate-300">
+                    {showBody && (
+                      <>
+                        {item["exercises"] &&
+                          item["exercises"].map((exercise) => {
+                            console.log(exercise.id);
+                            return (
+                              <tr key={exercise.id}>
+                                <td>
+                                  <button
+                                    onClick={() => openExerciseModal(exercise)}
+                                    className="text-secondary-200 underline"
+                                  >
+                                    {exercise.name}
+                                  </button>
+                                </td>
+                              </tr>
+                            );
+                          })}
+                      </>
+                    )}
+                  </tbody>
+                </table>
+              );
+            })}
+        </div>
 
-              <td>bbbb</td>
-
-              <td>
-                <button className="underline decoration-1 hover:text-secondary-300 ">
-                  ccccccccccccc
-                </button>
-              </td>
-
-              <td>ccccccccccccc</td>
-
-              <td>
-                <button className="text-red-200 text-center underline decoration-1 font-montserrat  font-normal hover:text-red-300">
-                  Scaduto
-                </button>
-              </td>
-
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+        <Modal
+          isOpen={selectedExercise !== null}
+          onRequestClose={closeModal}
+          className="bg-transparent z-10 border-none flex justify-center items-center"
+        >
+          <div
+            onClick={closeModal}
+            className="fixed inset-0 cursor-pointer"
+          ></div>
+          <div className="h-screen flex justify-center items-center">
+            {selectedExercise !== null && (
+              <img
+                src={selectedExercise.img}
+                alt={selectedExercise.name}
+                className="max-w-full content-center self-center text-center"
+              />
+            )}
+          </div>
+        </Modal>
       </div>
     </>
   );
