42a7ca6255b278bc911a72df7f6c6bdafb2aced5
Antonino Alampi <Alampiantonino99@gmail.com>
2023-09-12 14:28:13 +0200

    fix: cleaned repo

--- src/components/shared/SectionDivider.jsx
+++
@@ -1,7 +0,0 @@
-export const SectionDivider = () => {
-  return (
-    <div className="flex justify-center bg-primary">
-      <div className="max-w-section w-full bg-divider h-[1px] rounded-full opacity-50"></div>
-    </div>
-  );
-};
